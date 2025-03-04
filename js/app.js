a297ad9184a3b89c845c754ae810c837a25a5038
Kacper <kgrzesik17@gmail.com>
2025-03-04 16:10:00 +0100

    dodano animacje

--- js/app.js
+++ js/app.js
@@ -16,4 +16,18 @@ function smooth() {
     requestScroll = requestAnimationFrame(smooth);
 }
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    }));
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el => observer.observe(el)));
+
 smooth()
